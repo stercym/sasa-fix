17542bad6eea6835c8b9d8a2061be56818c81120
Micheni58 <michenicaleb@gmail.com>
2025-11-07 15:11:55 +0300

    Add about page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,14 +2,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
-
+import ProviderDashboard from "../pages/Serviceprovider/ProviderDash";
+import About from "../pages/About";
 function AppRouter() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        
+
+        <Route path="/pages/Serviceprovider/dashboard" element={<ProviderDashboard />}/>
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
