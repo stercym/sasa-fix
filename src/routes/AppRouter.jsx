e74ca91138a7f7810c7c59b4f4f6788e35d54d06
Micheni58 <michenicaleb@gmail.com>
2025-11-07 15:35:32 +0300

    Add routes to router file

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,6 +4,8 @@ import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import ProviderDashboard from "../pages/Serviceprovider/ProviderDash";
 import About from "../pages/About";
+import Login from "../components/Login/Login";
+import CreateAcc from "../components/CreateAcc/createAcc";
 function AppRouter() {
   return (
     <Router>
@@ -13,6 +15,8 @@ function AppRouter() {
 
         <Route path="/pages/Serviceprovider/dashboard" element={<ProviderDashboard />}/>
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<CreateAcc />}/>
       </Routes>
     </Router>
   );
